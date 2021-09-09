d7dde036b5cee3a4a5fde41f14b1dd05554ccfa6
Jordan Reed <jlreed@students.nic.edu>
2021-09-09 16:51:02 -0700

    Program now takes command line arguments as file input. Currently accepts one file at a time.

--- main.cpp
+++ main.cpp
@@ -2,10 +2,17 @@
 #include <fstream>
 #include <string>
 
-int main()
+int main(int argc, char **argv)
 {
-    //test file to open
-    std::string fileName = "testfile.cpp";
+    // give error if more than one file is passed through
+    if(argc > 2)
+    {
+        std::cerr << "Can't handle more than one file at a time.\n";
+        exit(1);
+    }
+
+    // file to open, taken from command line
+    std::string fileName = argv[1];
 
     // open file
     std::fstream inFile;
@@ -18,17 +25,18 @@ int main()
         exit(1);
     }
     
-    // change the file name
+    // change the file name for correct output file
     fileName += ".lexer.out";
 
     //create new output file
     std::fstream outFile;
     outFile.open(fileName, std::ios::out);
 
+    // loop through input file and put into output file
     std::string dummyString = "";
-
     while(!inFile.eof())
     {
+        // TO DO: find better way for reading from file. 
         // read from input file
         getline(inFile, dummyString);
 
@@ -36,6 +44,7 @@ int main()
         outFile << dummyString << "\n";
     }
 
+    // close files
     inFile.close();
     outFile.close();
 
