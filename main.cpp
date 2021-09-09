f0d329f75c46ac5f133ff736207cad28e42e3dc2
Jordan Reed <jlreed@students.nic.edu>
2021-09-09 16:33:54 -0700

    Program now will copy a specific file (specified in program) into another file with .lexer.out appended to the file name.

--- main.cpp
+++ main.cpp
@@ -5,24 +5,39 @@
 int main()
 {
     //test file to open
-    std::string testName = "testfile";
+    std::string fileName = "testfile.cpp";
 
     // open file
-    std::ifstream inFile(testName, std::ifstream::in);
-    // check if file didn't open
-    if(!inFile.is_open())
+    std::fstream inFile;
+    inFile.open(fileName, std::ios::in);
+
+    // check if file opened
+    if(!inFile)
     {
-        std::cout << "Error opening file.\n";
-        return 0;
+        std::cerr << "Error opening file\n";
+        exit(1);
+    }
+    
+    // change the file name
+    fileName += ".lexer.out";
+
+    //create new output file
+    std::fstream outFile;
+    outFile.open(fileName, std::ios::out);
+
+    std::string dummyString = "";
+
+    while(!inFile.eof())
+    {
+        // read from input file
+        getline(inFile, dummyString);
+
+        // output to new file
+        outFile << dummyString << "\n";
     }
 
-    // change the file name
-    testName += ".lexer.out";
-
-    //create output file
-    std::ofstream outFile(testName, std::ofstream::out);
-
-    std::cout << "test\n";
+    inFile.close();
+    outFile.close();
 
     return 0;
 }
