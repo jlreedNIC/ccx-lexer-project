154ff0701981c75172b01faad1cdeaf97509b941
Jordan Reed <jlreed@students.nic.edu>
2021-10-01 12:50:28 -0700

    added testing code.

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,14 @@ int main(int argc, char **argv)
         std::cerr << "Can't handle more than one file at a time.\n";
         exit(1);
     }
-    if(argc < 1)
+    if(argc <= 1)
     {
         std::cerr << "Please enter a filename.\n";
         exit(1);
     }
+
+    Parser parser(argv[1]);
+    parser.parse();
 
     // file to open, taken from command line
     std::string fileName = argv[1];
@@ -45,45 +48,45 @@ int main(int argc, char **argv)
     std::fstream outFile;
     outFile.open(fileName, std::ios::out);
 
-    // loop through input file and put into output file
-    std::string dummyString = "";
-    std::string placeHolder = "";
-    int size = 0;
-    while(!inFile.eof())
-    {
-        // TO DO: find better way for reading from file. 
-        // read from input file
-        inFile >> dummyString;
+    // // loop through input file and put into output file
+    // std::string dummyString = "";
+    // std::string placeHolder = "";
+    // int size = 0;
+    // while(!inFile.eof())
+    // {
+    //     // TO DO: find better way for reading from file. 
+    //     // read from input file
+    //     inFile >> dummyString;
 
-        // check for comments
-        if(dummyString[0] == '/' && dummyString[1] == '*')
-        {
-            // ignore next /n
-            inFile.ignore();
-            // comment found
-            do
-            {
-                // store comment in place holder until end of comment is found
-                placeHolder += dummyString + '\n';
-                // get next line of comment
-                getline(inFile, dummyString);
-                // check to see if end of comment is found
-                size = dummyString.length();
-            } while (dummyString[size-1] != '/' && dummyString[size-2] != '*');
+    //     // check for comments
+    //     if(dummyString[0] == '/' && dummyString[1] == '*')
+    //     {
+    //         // ignore next /n
+    //         inFile.ignore();
+    //         // comment found
+    //         do
+    //         {
+    //             // store comment in place holder until end of comment is found
+    //             placeHolder += dummyString + '\n';
+    //             // get next line of comment
+    //             getline(inFile, dummyString);
+    //             // check to see if end of comment is found
+    //             size = dummyString.length();
+    //         } while (dummyString[size-1] != '/' && dummyString[size-2] != '*');
 
-            // reset placeHolder and output dummyString
-            placeHolder += dummyString;
-            dummyString = placeHolder;
-            placeHolder = "";
-            // output comment to file
-            outFile << dummyString << " (comment)\n";
-        }
-        else
-        {
-            // output to new file
-            outFile << dummyString << "\n"; 
-        }  
-    }
+    //         // reset placeHolder and output dummyString
+    //         placeHolder += dummyString;
+    //         dummyString = placeHolder;
+    //         placeHolder = "";
+    //         // output comment to file
+    //         outFile << dummyString << " (comment)\n";
+    //     }
+    //     else
+    //     {
+    //         // output to new file
+    //         outFile << dummyString << "\n"; 
+    //     }  
+    // }
 
     // close files
     inFile.close();
